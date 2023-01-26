f71990fd073ad6ce4890c0d25c979c8b4be68301
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-01-25 19:41:28 -0500

    Feat: Categories, products and users routers implemented

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("My app");
+const express = require("express");
+const routerApi = require("./routes");
+const app = express();
+const port  = 3000;
+
+app.get("/", (req, res) =>{
+  res.send("Hi, server in Express.");
+});
+
+routerApi(app);
+
+app.listen(port, () => {
+  console.log("Running on port "+port);
+});
