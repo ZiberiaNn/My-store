640a35ccf3f8e5f9b4399986d5f49ef9efabebb2
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-02-12 14:05:10 -0500

    Fix: Product Schema to accept CategoryID

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -3,12 +3,14 @@ const Joi = require("joi");
 const id = Joi.number().integer();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer();
+const categoryId = Joi.number().integer();
 //const image = Joi.string().uri();
 //const isBlock = Joi.boolean();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  categoryId: categoryId
   //image: image.required(),
   //isBlock: isBlock.required()
 });
@@ -17,6 +19,7 @@ const updateProductSchema = Joi.object({
   id: id,
   name: name,
   price: price,
+  categoryId: categoryId,
   //image: image,
   //isBlock: isBlock
 });
