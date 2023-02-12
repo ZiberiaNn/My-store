03babee323a67b2b5f4051d407ef7c47943f65d7
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-02-11 23:31:58 -0500

    Fix: DB Engine environment variable

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,10 +4,10 @@ const setupModels = require("./../db/models");
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `${config.dbEngine}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: config.dbEngine,
   logging: console.log
 });
 
